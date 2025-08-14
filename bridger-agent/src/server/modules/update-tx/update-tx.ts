98e118f3b521173dd05ec816f6a5d40ac029ec78
Josh Roy <josh@pillar-advisors.com>
2025-08-14 16:33:25 -0400

    Adding transaction updates.

--- bridger-agent/src/server/modules/update-tx/update-tx.ts
+++ bridger-agent/src/server/modules/update-tx/update-tx.ts
@@ -1,13 +1,23 @@
 import { prisma } from "@/lib/db";
 
 export const updateTx = async () => {
-  const greet = await prisma.greet.create({});
+  const transaction = await prisma.transaction.findUniqueOrThrow({
+    where: { id: 1 },
+  });
+
+  const updatedTransaction = await prisma.transaction.update({
+    where: { id: transaction.id },
+    data: {
+      pickedVendor: "Amazon",
+      pickedCategory: "Groceries",
+    },
+  });
+
   const response = await fetch("http://localhost:8000/transactions", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ username: greet.id.toLocaleString() }),
   });
   return await response.json();
 };
