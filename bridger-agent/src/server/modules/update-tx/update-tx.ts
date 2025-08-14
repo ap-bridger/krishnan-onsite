25d5f059589fcaaa57602f8ccfb17b6cbf86eb69
pkrish82 <pkrish82@gmail.com>
2025-08-14 14:39:43 -0400

    Update update-tx.ts

--- bridger-agent/src/server/modules/update-tx/update-tx.ts
+++ bridger-agent/src/server/modules/update-tx/update-tx.ts
@@ -1,1 +1,13 @@
-abc
+import { prisma } from "@/lib/db";
+
+export const updateTx = async () => {
+  const greet = await prisma.greet.create({});
+  const response = await fetch("http://localhost:8000/transactions", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username: greet.id.toLocaleString() }),
+  });
+  return await response.json();
+};
